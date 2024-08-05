2eadc6123a01a5bf40ba434e27c6b5fdbf64a660
vivy33 <vivymomo0407@Gmail.com>
2024-08-06 00:43:45 +0800

    update

--- RingBuffer/shared_memory.c
+++ RingBuffer/shared_memory.c
@@ -3,12 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#ifndef MAP_ANONYMOUS
-#define MAP_ANONYMOUS 0x20
-#endif
-
 RingBuffer* create_anonymous_shared_memory(size_t size) {
-    void* addr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    void* addr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, -1, 0);
     if (addr == MAP_FAILED) {
         perror("mmap");
         exit(1);
