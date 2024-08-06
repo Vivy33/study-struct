f112762bc0a608bf399ea7abf8701e114d18f9fe
vivy33 <vivymomo0407@Gmail.com>
2024-08-07 00:02:40 +0800

    update

--- ELF/main.c
+++ ELF/main.c
@@ -1,23 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <errno.h>
 #include <string.h>
 #include "elf.h"
 
+// 打印 ELF 文件头信息
 void print_elf_header(const Elf64_Ehdr *header) {
-    // Add your implementation to print ELF header details
+    printf("ELF Header:\n");
+    printf("  Type: %u\n", header->e_type);
+    printf("  Machine: %u\n", header->e_machine);
+    printf("  Version: %u\n", header->e_version);
+    printf("  Entry point address: 0x%lx\n", header->e_entry);
+    printf("  Start of program headers: %lu (bytes into file)\n", header->e_phoff);
+    printf("  Start of section headers: %lu (bytes into file)\n", header->e_shoff);
+    printf("  Flags: 0x%x\n", header->e_flags);
+    printf("  Size of this header: %u (bytes)\n", header->e_ehsize);
+    printf("  Size of program headers: %u (bytes)\n", header->e_phentsize);
+    printf("  Number of program headers: %u\n", header->e_phnum);
+    printf("  Size of section headers: %u (bytes)\n", header->e_shentsize);
+    printf("  Number of section headers: %u\n", header->e_shnum);
+    printf("  Section header string table index: %u\n", header->e_shstrndx);
 }
 
-void print_program_headers(const Elf64_Phdr *phdrs, int phnum) {
-    // Add your implementation to print program headers
+// 打印程序头
+void print_program_headers(const Elf64_Phdr *phdrs, uint16_t phnum) {
+    printf("Program Headers:\n");
+    for (int i = 0; i < phnum; ++i) {
+        printf("  Type: %u\n", phdrs[i].p_type);
+        printf("  Flags: 0x%x\n", phdrs[i].p_flags);
+        printf("  Offset: 0x%lx\n", phdrs[i].p_offset);
+        printf("  Virtual Address: 0x%lx\n", phdrs[i].p_vaddr);
+        printf("  Physical Address: 0x%lx\n", phdrs[i].p_paddr);
+        printf("  File Size: %lu\n", phdrs[i].p_filesz);
+        printf("  Memory Size: %lu\n", phdrs[i].p_memsz);
+        printf("  Alignment: %lu\n", phdrs[i].p_align);
+    }
 }
 
-void print_section_headers(const Elf64_Shdr *shdrs, int shnum) {
-    // Add your implementation to print section headers
+// 打印节头
+void print_section_headers(const Elf64_Shdr *shdrs, uint16_t shnum) {
+    printf("Section Headers:\n");
+    for (int i = 0; i < shnum; ++i) {
+        printf("  Name: %u\n", shdrs[i].sh_name);
+        printf("  Type: %u\n", shdrs[i].sh_type);
+        printf("  Flags: 0x%lx\n", shdrs[i].sh_flags);
+        printf("  Address: 0x%lx\n", shdrs[i].sh_addr);
+        printf("  Offset: %lu\n", shdrs[i].sh_offset);
+        printf("  Size: %lu\n", shdrs[i].sh_size);
+        printf("  Link: %u\n", shdrs[i].sh_link);
+        printf("  Info: %u\n", shdrs[i].sh_info);
+        printf("  Address Alignment: %lu\n", shdrs[i].sh_addralign);
+        printf("  Entry Size: %lu\n", shdrs[i].sh_entsize);
+    }
 }
 
+// 读取 ELF 文件并进行解析
 void read_elf_file(const char *filename) {
     int fd = open(filename, O_RDONLY);
     if (fd == -1) {
@@ -25,50 +64,53 @@ void read_elf_file(const char *filename) {
         exit(EXIT_FAILURE);
     }
 
-    // Read ELF header
+    // 读取 ELF 文件头
     Elf64_Ehdr header;
-    if (read(fd, &header, sizeof(header)) != sizeof(header)) {
-        perror("read");
+    if (pread(fd, &header, sizeof(header), 0) != sizeof(header)) {
+        perror("pread");
         close(fd);
         exit(EXIT_FAILURE);
     }
 
+    // 打印 ELF 文件头信息
     print_elf_header(&header);
 
-    // Read Program Headers
-    if (lseek(fd, header.e_phoff, SEEK_SET) == -1) {
-        perror("lseek");
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
+    // 读取并打印程序头
     Elf64_Phdr *phdrs = malloc(header.e_phnum * sizeof(Elf64_Phdr));
-    if (read(fd, phdrs, header.e_phnum * sizeof(Elf64_Phdr)) != header.e_phnum * sizeof(Elf64_Phdr)) {
-        perror("read");
+    if (pread(fd, phdrs, header.e_phnum * sizeof(Elf64_Phdr), header.e_phoff) != header.e_phnum * sizeof(Elf64_Phdr)) {
+        perror("pread");
         free(phdrs);
         close(fd);
         exit(EXIT_FAILURE);
     }
-
     print_program_headers(phdrs, header.e_phnum);
     free(phdrs);
 
-    // Read Section Headers
-    if (lseek(fd, header.e_shoff, SEEK_SET) == -1) {
-        perror("lseek");
+    // 读取并打印节头
+    Elf64_Shdr *shdrs = malloc(header.e_shnum * sizeof(Elf64_Shdr));
+    if (pread(fd, shdrs, header.e_shnum * sizeof(Elf64_Shdr), header.e_shoff) != header.e_shnum * sizeof(Elf64_Shdr)) {
+        perror("pread");
+        free(shdrs);
         close(fd);
         exit(EXIT_FAILURE);
     }
-    Elf64_Shdr *shdrs = malloc(header.e_shnum * sizeof(Elf64_Shdr));
-    if (read(fd, shdrs, header.e_shnum * sizeof(Elf64_Shdr)) != header.e_shnum * sizeof(Elf64_Shdr)) {
-        perror("read");
+    print_section_headers(shdrs, header.e_shnum);
+
+    // 读取节头字符串表
+    char *shstrtab = malloc(shdrs[header.e_shstrndx].sh_size);
+    if (pread(fd, shstrtab, shdrs[header.e_shstrndx].sh_size, shdrs[header.e_shstrndx].sh_offset) != shdrs[header.e_shstrndx].sh_size) {
+        perror("pread");
+        free(shstrtab);
         free(shdrs);
         close(fd);
         exit(EXIT_FAILURE);
     }
 
-    print_section_headers(shdrs, header.e_shnum);
-    free(shdrs);
+    // 打印符号表
+    print_symbol_table(fd, shdrs, header.e_shnum, shstrtab);
 
+    free(shstrtab);
+    free(shdrs);
     close(fd);
 }
 
@@ -78,6 +120,8 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
+    // 读取并解析 ELF 文件
     read_elf_file(argv[1]);
     return EXIT_SUCCESS;
 }
+
