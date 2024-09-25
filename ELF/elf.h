250b5660c2e9c39ca80fdb56df3b03c123bf11ef
vivy33 <vivymomo0407@Gmail.com>
2024-09-25 21:22:24 +0800

    简化

--- ELF/elf.h
+++ ELF/elf.h
@@ -10,11 +10,4 @@
 //对齐到4字节边界是为了确保数据在内存中的存放符合某些硬件平台的对齐要求，从而提高内存访问的效率和正确性。
 #define NOTE_ALIGN(n) (((n) + 3) & -4U)
 
-void print_elf_header(const Elf64_Ehdr *header);
-void print_program_headers(const Elf64_Phdr *phdrs, uint16_t phnum);
-void print_section_headers(const Elf64_Shdr *shdrs, uint16_t shnum);
-void print_symbol_table(int fd, Elf64_Shdr *shdrs, uint16_t shnum, char *shstrtab);
-void print_symbol_table(int fd, Elf64_Shdr *shdrs, uint16_t shnum, char *shstrtab);
-void find_symbol_by_address(int fd, Elf64_Shdr *shdrs, uint16_t shnum, char *shstrtab, uint64_t address);
-
 #endif // ELF_H
