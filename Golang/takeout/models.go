97697c2f9b3814a30bffd945151a453f1c614c55
vivy33 <vivymomo0407@Gmail.com>
2024-11-01 01:27:19 +0800

    update

--- Golang/takeout/models.go
+++ Golang/takeout/models.go
@@ -17,13 +17,13 @@ type Rider struct {
 	RiderID     int     `json:"rider_id"`     // 骑手ID，区分于 UserID
 	VehicleType string  `json:"vehicle_type"` // 骑手使用的交通工具类型
 	Rating      float64 `json:"rating"`       // 骑手的评分
-	Status      string  `json:"status"`       // 骑手状态（如在线、休息、离线）
+	RiderStatus string  `json:"riderstatus"`  // 骑手状态（如在线、休息、离线）
 }
 
 // 商家结构体
 type Shop struct {
 	ShopID      int    `json:"shop_id"`
-	Name        string `json:"name"`
+	ShopName    string `json:"shopname"`
 	Phone       string `json:"phone"`
 	Address     string `json:"address"`
 	Description string `json:"description"`
@@ -33,7 +33,7 @@ type Shop struct {
 type Product struct {
 	ProductID   int     `json:"product_id"`
 	ShopID      int     `json:"shop_id"`
-	Name        string  `json:"name"`
+	ProductName string  `json:"name"`
 	Description string  `json:"description"`
 	Price       float64 `json:"price"`
 	Stock       int     `json:"stock"`
@@ -41,17 +41,28 @@ type Product struct {
 
 // 订单结构体
 type Order struct {
-	OrderID    int       `json:"order_id"`
-	UserID     int       `json:"user_id"`
-	ShopID     int       `json:"shop_id"`
-	Status     string    `json:"status"`
-	OrderTime  time.Time `json:"order_time"`
-	TotalPrice float64   `json:"total_price"`
+	OrderID     int       `json:"order_id"`
+	UserID      int       `json:"user_id"`
+	ShopID      int       `json:"shop_id"`
+	RiderID     int       `json:"rider_id"`
+	ProductID   int       `json:"product_id"`
+	OrderStatus string    `json:"orderstatus"`
+	OrderTime   time.Time `json:"order_time"`
+	TotalPrice  float64   `json:"total_price"`
+	GroupID     int       `json:"group_id,omitempty"`
+}
+
+// Group
+type Group struct {
+	GroupID int `json:"group_id,omitempty"`
+	OrderID int `json:"order_id"`
+	UserID  int `json:"user_id"`
+	ShopID  int `json:"shop_id"`
+	RiderID int `json:"rider_id"`
 }
 
 // Message 表示一条群聊消息的结构体
 type Message struct {
-	ID        int       `json:"id"`        // 消息ID
 	GroupID   int       `json:"group_id"`  // 群组ID
 	SenderID  int       `json:"sender_id"` // 发送者ID
 	Content   string    `json:"content"`   // 消息内容
