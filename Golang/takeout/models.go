c864515f5d1348145f968950813ceb43e1a7b7c8
vivy33 <vivymomo0407@Gmail.com>
2024-11-04 20:37:06 +0800

    update

--- Golang/takeout/models.go
+++ Golang/takeout/models.go
@@ -17,13 +17,13 @@ type Rider struct {
 	RiderID     int     `json:"rider_id"`     // 骑手ID，区分于 UserID
 	VehicleType string  `json:"vehicle_type"` // 骑手使用的交通工具类型
 	Rating      float64 `json:"rating"`       // 骑手的评分
-	RiderStatus string  `json:"riderstatus"`  // 骑手状态（如在线、休息、离线）
+	RiderStatus string  `json:"rider_status"` // 骑手状态（如在线、休息、离线）
 }
 
 // 商家结构体
 type Shop struct {
 	ShopID       int    `json:"shop_id"`
-	ShopName     string `json:"shopname"`
+	ShopName     string `json:"shop_name"`
 	ShopPassword string `json:"shop_password"`
 	Phone        string `json:"phone"`
 	Address      string `json:"address"`
@@ -34,7 +34,7 @@ type Shop struct {
 type Product struct {
 	ProductID   int     `json:"product_id"`
 	ShopID      int     `json:"shop_id"`
-	ProductName string  `json:"name"`
+	ProductName string  `json:"product_name"`
 	Description string  `json:"description"`
 	Price       float64 `json:"price"`
 	Stock       int     `json:"stock"`
@@ -47,8 +47,11 @@ type Order struct {
 	ShopID      int       `json:"shop_id"`
 	RiderID     int       `json:"rider_id"`
 	ProductID   int       `json:"product_id"`
-	OrderStatus string    `json:"orderstatus"`
+	OrderStatus string    `json:"order_status"`
+	Username    string    `json:"username"`
+	ShopName    string    `json:"shop_name"`
 	OrderTime   time.Time `json:"order_time"`
+	ProductName string    `json:"product_name"`
 	TotalPrice  float64   `json:"total_price"`
 	GroupID     int       `json:"group_id,omitempty"`
 }
@@ -64,9 +67,10 @@ type Group struct {
 
 // Message 表示一条群聊消息的结构体
 type Message struct {
-	MessageID int       `json:"message_id"` // 某条消息的ID
-	GroupID   int       `json:"group_id"`   // 群组ID
-	SenderID  int       `json:"sender_id"`  // 发送者ID
-	Content   string    `json:"content"`    // 消息内容
-	Timestamp time.Time `json:"timestamp"`  // 消息时间戳
+	MessageID  int       `json:"message_id"`  // 某条消息的ID
+	GroupID    int       `json:"group_id"`    // 群组ID
+	SenderID   int       `json:"sender_id"`   // 发送者ID
+	SenderName string    `json:"sender_name"` // "user", "shop", "rider"
+	Content    string    `json:"content"`     // 消息内容
+	Timestamp  time.Time `json:"timestamp"`   // 消息时间戳
 }
